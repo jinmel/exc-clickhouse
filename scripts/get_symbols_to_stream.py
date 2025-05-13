86726afb530edd3135467ff99137a8425f9ae35e
Jinsuk Park <totorovirus@gmail.com>
2025-05-13 19:10:29 +0900

    filter more pairs

--- scripts/get_symbols_to_stream.py
+++ scripts/get_symbols_to_stream.py
@@ -26,8 +26,10 @@ def parse_args():
 
 
 def main():
+
+    quote_asset_to_use = ['USDT', 'USDC', 'BTC', 'ETH']
     args = parse_args()
-    
+
     # Read symbols from address_symbols.tsv
     symbols = set()
     with open(args.address_symbols, 'r') as f:
@@ -36,28 +38,30 @@ def main():
         for row in reader:
             if row:  # Make sure row is not empty
                 symbols.add(row[0])  # Add symbol from column 0
-    
+
     print(f"Loaded {len(symbols)} symbols from {args.address_symbols}")
-    
+
     # Filter trading pairs containing those symbols
     filtered_pairs = []
     with open(args.trading_pairs, 'r') as f:
         reader = csv.reader(f, delimiter='\t')
-        
+
         for row in reader:
             if row:  # Make sure row is not empty
+                if row[1] != 'SPOT':
+                    continue
                 base_asset = row[3]
                 quote_asset = row[4]
-                
+
                 for symbol in symbols:
-                    if symbol == base_asset:
+                    if symbol == base_asset and quote_asset in quote_asset_to_use:
                         filtered_pairs.append([row[2]])
-    
+
     # Write filtered pairs to output file
     with open(args.output, 'w', newline='') as f:
         writer = csv.writer(f, delimiter='\t')
         writer.writerows(filtered_pairs)
-    
+
     print(f"Filtered {len(filtered_pairs)-1} trading pairs to {args.output}")
 
 
