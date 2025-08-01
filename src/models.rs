0c92780f56816dda766c85c759cdf55b4e215aa4
Jinsuk Park <totorovirus@gmail.com>
2025-08-01 23:14:57 +0900

    Binance Futures Stream Implementation

--- src/models.rs
+++ src/models.rs
@@ -8,6 +8,8 @@ use crate::{ethereum::BlockMetadata, timeboost::bids::BidData};
 pub enum ExchangeName {
     #[serde(rename = "binance")]
     Binance,
+    #[serde(rename = "binancefutures")]
+    BinanceFutures,
     #[serde(rename = "bybit")]
     Bybit,
     #[serde(rename = "okex")]
@@ -26,6 +28,7 @@ impl std::fmt::Display for ExchangeName {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             ExchangeName::Binance => write!(f, "binance"),
+            ExchangeName::BinanceFutures => write!(f, "binancefutures"),
             ExchangeName::Bybit => write!(f, "bybit"),
             ExchangeName::Okx => write!(f, "okex"),
             ExchangeName::Coinbase => write!(f, "coinbase"),
